8df4a86a2b114e0dda66bee11b6024882fc2d8e9
William <will27528@gmail.com>
2022-02-09 17:11:13 +0000

    Cleanup ProfanityFilterer code to rely on a runnable with CompletableFutures rather than extended thread

--- common/src/main/java/net/william278/huskchat/filter/ProfanityFilterer.java
+++ common/src/main/java/net/william278/huskchat/filter/ProfanityFilterer.java
@@ -2,6 +2,8 @@ package net.william278.huskchat.filter;
 
 import net.william278.profanitycheckerapi.ProfanityChecker;
 
+import java.util.concurrent.*;
+
 /**
  * A {@link ChatFilter} that filters against profanity using machine learning
  * Uses <a href="https://github.com/WiIIiam278/ProfanityCheckerAPI/">ProfanityCheckerAPI</a>, which uses jep to run a python
@@ -9,19 +11,27 @@ import net.william278.profanitycheckerapi.ProfanityChecker;
  */
 public class ProfanityFilterer extends ChatFilter {
 
-    private ProfanityChecker profanityChecker;
-    private final ProfanityFilterMode filterMode;
     private final double thresholdValue;
 
+    private final ProfanityFilterMode profanityFilterMode;
+
+    private final ProfanityFiltererRunnable profanityFiltererRunnable;
+
     public ProfanityFilterer(ProfanityFilterMode filterMode, double thresholdValue) {
-        this.filterMode = filterMode;
         this.thresholdValue = thresholdValue;
-        new Thread(() -> profanityChecker = new ProfanityChecker()).start();
+        this.profanityFilterMode = filterMode;
+        this.profanityFiltererRunnable = new ProfanityFiltererRunnable();
+        new Thread(profanityFiltererRunnable).start();
     }
 
     @Override
     public boolean isAllowed(String message) {
-        return filterProfanity(message);
+        try {
+            return !getIsProfane(message);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
@@ -35,11 +45,10 @@ public class ProfanityFilterer extends ChatFilter {
      * @param message The message to check
      * @return {@code true} if the message is profane
      */
-    private boolean filterProfanity(String message) {
-        if (profanityChecker == null) return false;
-        return switch (filterMode) {
-            case AUTOMATIC -> profanityChecker.isTextProfane(message);
-            case TOLERANCE -> profanityChecker.getTextProfanityLikelihood(message) >= thresholdValue;
+    private boolean getIsProfane(String message) throws ExecutionException, InterruptedException {
+        return switch (profanityFilterMode) {
+            case AUTOMATIC -> profanityFiltererRunnable.calculateIsProfane(message);
+            case TOLERANCE -> profanityFiltererRunnable.calculateIsProfane(message, thresholdValue);
         };
     }
 
@@ -47,8 +56,78 @@ public class ProfanityFilterer extends ChatFilter {
      * Dispose of the ProfanityChecker instance
      */
     public void dispose() {
-        if (profanityChecker != null) {
-            profanityChecker.dispose();
+        profanityFiltererRunnable.threadState = ProfanityFiltererRunnable.ThreadState.TERMINATED;
+    }
+
+    private static class ProfanityFiltererRunnable implements Runnable {
+
+        private ProfanityChecker profanityChecker;
+
+        private ThreadState threadState;
+        private ProfanityFilterMode profanityFilterMode;
+        private String input;
+        private CompletableFuture<Object> output;
+
+        public ProfanityFiltererRunnable() {
+            threadState = ThreadState.SETUP;
+        }
+
+        public boolean calculateIsProfane(String text) {
+            try {
+                return (boolean) calculate(text, ProfanityFilterMode.AUTOMATIC).get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+                return new CompletableFuture<>().complete(false);
+            }
+        }
+
+        public boolean calculateIsProfane(String text, double threshold) {
+            try {
+                return ((double) calculate(text, ProfanityFilterMode.TOLERANCE).get()) > threshold;
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+                return new CompletableFuture<>().complete(false);
+            }
+        }
+
+        private CompletableFuture<Object> calculate(String text, ProfanityFilterMode mode) {
+            output = new CompletableFuture<>();
+            profanityFilterMode = mode;
+            input = text;
+            threadState = ThreadState.CALCULATE;
+            return output;
+        }
+
+        @Override
+        public void run() {
+            ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+            executor.scheduleAtFixedRate(() -> {
+                switch (threadState) {
+                    case SETUP -> {
+                        profanityChecker = new ProfanityChecker();
+                        threadState = ThreadState.ASLEEP;
+                    }
+                    case CALCULATE -> {
+                        if (profanityFilterMode == ProfanityFilterMode.AUTOMATIC) {
+                            output.complete(profanityChecker.isTextProfane(input));
+                        } else if (profanityFilterMode == ProfanityFilterMode.TOLERANCE) {
+                            output.complete(profanityChecker.getTextProfanityLikelihood(input));
+                        }
+                        threadState = ThreadState.ASLEEP;
+                    }
+                    case TERMINATED -> {
+                        profanityChecker.dispose();
+                        executor.shutdown();
+                    }
+                }
+            }, 0, 20, TimeUnit.MILLISECONDS);
+        }
+
+        public enum ThreadState {
+            ASLEEP,
+            SETUP,
+            CALCULATE,
+            TERMINATED
         }
     }
 
