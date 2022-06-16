6dda91fe1663531a821937aaadad67beafda0883
TrueWinter <nicholis@truewinter.dev>
2022-06-16 21:20:28 +0200

    that can probably use the same system as bungee

--- velocity/src/main/java/net/william278/huskchat/velocity/event/BaseEvent.java
+++ velocity/src/main/java/net/william278/huskchat/velocity/event/BaseEvent.java
@@ -1,30 +1,17 @@
 package net.william278.huskchat.velocity.event;
 
-import com.velocitypowered.api.event.ResultedEvent;
 import net.william278.huskchat.event.EventBase;
 
-import java.util.Objects;
-
-public class BaseEvent implements ResultedEvent<ResultedEvent.GenericResult>, EventBase {
-    private ResultedEvent.GenericResult result = ResultedEvent.GenericResult.allowed();
-
-    @Override
-    public GenericResult getResult() {
-        return result;
-    }
-
-    @Override
-    public void setResult(GenericResult result) {
-        this.result = Objects.requireNonNull(result);
-    }
+public class BaseEvent implements EventBase {
+    private boolean cancelled = false;
 
     @Override
     public void setCancelled(boolean cancelled) {
-        this.result = cancelled ? GenericResult.denied() : GenericResult.allowed();
+        this.cancelled = cancelled;
     }
 
     @Override
     public boolean isCancelled() {
-        return !result.isAllowed();
+        return cancelled;
     }
 }
