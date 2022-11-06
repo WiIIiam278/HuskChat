ce17fa4ad1f9a121348603986a51b71a4554fde8
William278 <will27528@gmail.com>
2022-11-06 12:02:14 +0000

    Bump to ProfanityCheckerAPI v1.3, update tests to use the filter itself

--- common/src/test/java/net/william278/huskchat/filter/ProfanityFilterTests.java
+++ common/src/test/java/net/william278/huskchat/filter/ProfanityFilterTests.java
@@ -1,22 +1,39 @@
 package net.william278.huskchat.filter;
 
-import net.william278.profanitycheckerapi.ProfanityChecker;
+import net.william278.huskchat.player.Player;
+import net.william278.huskchat.player.TestPlayer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ProfanityFilterTests {
 
+    private final ProfanityFilterer filterer = new ProfanityFilterer(
+            ProfanityFilterer.ProfanityFilterMode.TOLERANCE, 0.8d, null);
+
     @Test
-    public void testProfanityFilter() {
-        try (ProfanityChecker checker = new ProfanityChecker()) {
-            Assertions.assertTrue(checker.isTextProfane("This is a fucking test sentence"));
-        }
+    public void givenSentenceContainingProfanity_testIsProfane() {
+        final Player dummyPlayer = new TestPlayer();
+        Assertions.assertTrue(filterer.isAllowed(dummyPlayer, "This is a fucking test sentence"));
+        Assertions.assertTrue(filterer.isAllowed(dummyPlayer, "Shit"));
     }
 
     @Test
-    public void testProfanityThreshold() {
-        try (ProfanityChecker checker = new ProfanityChecker()) {
-            Assertions.assertTrue(checker.getTextProfanityLikelihood("Fuck shit ass") > 0.95d);
-        }
+    public void givenNormalSentences_testIsNotProfane() {
+        final Player dummyPlayer = new TestPlayer();
+        Assertions.assertTrue(filterer.isAllowed(dummyPlayer, "AHOJ"));
+        Assertions.assertTrue(filterer.isAllowed(dummyPlayer, "Hello"));
+    }
+
+    @Test
+    public void givenObfuscatedProfanity_testIsProfane() {
+        final Player dummyPlayer = new TestPlayer();
+        Assertions.assertTrue(filterer.isAllowed(dummyPlayer, "Sh1tface"));
+        Assertions.assertTrue(filterer.isAllowed(dummyPlayer, "Sh1tf4ce"));
+    }
+
+    @Test
+    public void givenScunthorpe_testIsNotProfane() {
+        final Player dummyPlayer = new TestPlayer();
+        Assertions.assertFalse(filterer.isAllowed(dummyPlayer, "Scunthorpe"));
     }
 }
